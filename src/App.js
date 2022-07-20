bf7e45e5360ec00d3de5709103316aba8f5cb951
bigish123 <102987684+bigish123@users.noreply.github.com>
2022-07-19 22:41:34 -0400

    new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ListOfBasicInfo from './pages/HARD.jsx';
+
+const App = () => {
+  return (
+    <div className= "List">
+      <ListOfBasicInfo />
+    </div>
+  )};
+
+  export default App;
